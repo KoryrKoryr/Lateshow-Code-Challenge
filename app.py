3976cc148617932b1ad478bb15e1e15f3f549788
Denis Korir <denohrkoryr@gmail.com>
2024-10-14 19:18:51 +0300

    Initial migration to create database schema

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db, Episode, Guest, Appearance
 from flask_migrate import Migrate
 
 # Initialize the app
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lateshow.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database and migration tool
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
