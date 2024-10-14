42faed0b92b672d0610b024a43e0770bc606a43b
Denis Korir <denohrkoryr@gmail.com>
2024-10-14 19:05:45 +0300

    Created models for Episode, Guest and Appearance with relationships and validations

---
+++ models.py
@@ -0,0 +1,61 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+
+#Episode model
+class Episode(db.Model, SerializerMixin):
+    __tablename__ = 'episodes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False, unique=True)
+
+    # Relationship with appearances
+    appearances = db.relationship('Appearance', backref='episode', cascade="all, delete-orphan")
+    
+    # Serialization rules to avoid recursion
+    serialize_rules = ('-appearances.episode',)
+
+    def __repr__(self):
+        return f'<Episode {self.id} - {self.number}>'
+
+#Guest model
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+
+    # Relationship with appearances
+    appearances = db.relationship('Appearance', backref='guest', cascade="all, delete-orphan")
+    
+    # Serialization rules to avoid recursion
+    serialize_rules = ('-appearances.guest',)
+
+    def __repr__(self):
+        return f'<Guest {self.id} - {self.name}>'
+
+#Appearance model
+class Appearance(db.Model, SerializerMixin):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+
+    # Serialization rules to include related episode and guest
+    serialize_rules = ('-episode.appearances', '-guest.appearances')
+
+    # Validation for rating (must be between 1 and 5)
+    @db.validates('rating')
+    def validate_rating(self, key, value):
+        if not (1 <= value <= 5):
+            raise ValueError('Rating must be between 1 and 5')
+        return value
+    
+    def __repr__(self):
+        return f'<Appearance {self.id} - Rating: {self.rating}>'
